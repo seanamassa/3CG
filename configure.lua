c6beabcb44c309ccea7cb40ed9441e08bff43569
seanamassa <69995662+seanamassa@users.noreply.github.com>
2025-05-30 13:51:36 -0700

    Delete configure.lua

--- configure.lua
+++
@@ -1,8 +0,0 @@
--- configure
-function love.conf(t)
-    t.window.title = "Greek Mythology 3CG"
-    t.window.width = 1280
-    t.window.height = 720
-    t.modules.joystick = false
-    t.modules.physics = false
-end
